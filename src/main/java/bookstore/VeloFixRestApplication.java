f7122296887cde6b9b9c5958dfec386af6e709ba
Maksym Shkarbun <suprime28@yahoo.com>
2025-01-10 13:38:44 +0200

    Init commit

---
+++ src/main/java/bookstore/VeloFixRestApplication.java
@@ -0,0 +1,15 @@
+package bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloFixRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeloFixRestApplication.class, args);
+    }
+}
+
+
+
