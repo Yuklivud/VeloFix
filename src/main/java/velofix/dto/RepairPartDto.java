ca0ba926d0e86a2c8ec03650f48513990c9a440a
Maksym Shkarbun <suprime28@yahoo.com>
2025-05-23 04:05:43 +0300

    Hot fixes

--- src/main/java/velofix/dto/RepairPartDto.java
+++ src/main/java/velofix/dto/RepairPartDto.java
@@ -1,10 +1,46 @@
 package velofix.dto;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class RepairPartDto {
     private UUID partId;
     private Integer quantityUsed;
+    private String partName;
+    private BigDecimal partPrice;
+    private BigDecimal sum;
+
+    public void calculateSum() {
+        if (partPrice != null && quantityUsed != null) {
+            this.sum = partPrice.multiply(BigDecimal.valueOf(quantityUsed));
+        } else {
+            this.sum = BigDecimal.ZERO;
+        }
+    }
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public BigDecimal getPartPrice() {
+        return partPrice;
+    }
+
+    public void setPartPrice(BigDecimal partPrice) {
+        this.partPrice = partPrice;
+    }
+
+    public void setSum(BigDecimal sum) {
+        this.sum = sum;
+    }
 
     public UUID getPartId() {
         return partId;
